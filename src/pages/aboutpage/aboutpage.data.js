2153cd24273d679009c82ebeb7f952b82fc47f25
Apoorv-17 <apoorv.ydv17@gmail.com>
2021-03-21 00:12:25 +0530

    Reordering the card list

--- src/pages/aboutpage/aboutpage.data.js
+++ src/pages/aboutpage/aboutpage.data.js
@@ -2,7 +2,7 @@ const team_members = [
   {
     id: 1,
     name: "Shushant Kumar Singh",
-    Job: "React Developer, Unity Game Developer - Weapons and Power Designer",
+    Job: "React Developer, Unity Game Developer -Weapons and Power Designer",
     email: "mailto:shushantkumar.singh2019@vitstudent.ac.in",
     github: "https://github.com/Shush25",
     linkdin: "https://www.linkedin.com/in/shushant-kumar-singh-bb016b1b6/",
@@ -13,7 +13,7 @@ const team_members = [
   {
     id: 2,
     name: "Apoorv Yadav",
-    Job: "React Developer, Unity Game Developer - Graphics and Level Designer",
+    Job: "React Developer, Unity Game Developer -Graphics and Level Designer",
     email: "mailto:apoorv.yadav2019@vitstudent.ac.in",
     github: "https://github.com/Apoorv-17",
     linkdin: "https://www.linkedin.com/in/apoorv-yadav-1a54571b7/",
@@ -23,6 +23,16 @@ const team_members = [
   },
   {
     id: 3,
+    name: "Divyanu Baheti",
+    Job: "Unity Game Developer - Graphics and Level Designer",
+    github: "https://github.com/Divyanu",
+    linkdin: "https://www.linkedin.com/in/divyanu-baheti-23a5a0185",
+    insta: "",
+    email: "mailto:divyanu.baheti2019@vitstudent.ac.in",
+    imageUrl: "https://www.w3schools.com/howto/img_avatar.png",
+  },
+  {
+    id: 4,
     name: "Rishi Mungia",
     Job: "Unity Game Developer - Main Character Designer",
     github: "https://github.com/rishimungia",
@@ -33,28 +43,7 @@ const team_members = [
       "https://lh3.googleusercontent.com/pw/ACtC-3c4cg49DSJfZ5KOYAEuipus930DTLm8eszaUZZc0W7jIC1VWuiQ4wwRpDAIDwSwGMkkXJuwSasnquuNlpRCUMFDIrY0gvELxU1_NKl1500ZmqBwX2J1Cxqckc36BrxZ_8YrtgU9xV-FpNQmSNmPtibM=s657-no?authuser=0",
   },
   {
-    id: 4,
-    name: "Karthikeyan Sugavanan",
-    Job: "Unity Game Developer - Enemy Type 3 Designer",
-    github: "https://github.com/KarthikeyanSugavanan",
-    linkdin: "https://www.linkedin.com/in/karthikeyan-sugavanan-01677718b",
-    insta: "",
-    email: "mailto:karthikeyan.sugavanan2019@vitstudent.ac.in",
-    imageUrl:
-      "https://lh3.googleusercontent.com/pw/ACtC-3fCmFsxzjQBMPxEnQQUwz4s7Kikc51BN65R4tt-C2glSys-dzgHSz3_HpupRgFePE7s2cS5uDGda5VgCbejwKxj6u0kObrrNdW7M4I9hweDbGObM4XJ1fQIqOXYy9HIx6kD489GE-LmZzNgiHudlmoB",
-  },
-  {
     id: 5,
-    name: "Divyanu Baheti",
-    Job: "Unity Game Developer - Graphics and Level Designer",
-    github: "https://github.com/Divyanu",
-    linkdin: "https://www.linkedin.com/in/divyanu-baheti-23a5a0185",
-    insta: "",
-    email: "mailto:divyanu.baheti2019@vitstudent.ac.in",
-    imageUrl: "https://www.w3schools.com/howto/img_avatar.png",
-  },
-  {
-    id: 6,
     name: "Ashish Das",
     Job: "Unity Game Developer - Enemy Type 1 Designer",
     github: "https://github.com/Sugar3201",
@@ -64,7 +53,7 @@ const team_members = [
     imageUrl: "https://www.w3schools.com/howto/img_avatar.png",
   },
   {
-    id: 7,
+    id: 6,
     name: "Rupak",
     Job: "Unity Game Developer - Enemy Type 2 Designer",
     github: "https://github.com/rupak-20",
@@ -73,5 +62,16 @@ const team_members = [
     email: "mailto:rupak.swami2019@vitstudent.ac.in",
     imageUrl: "https://www.w3schools.com/howto/img_avatar.png",
   },
+  {
+    id: 7,
+    name: "Karthikeyan Sugavanan",
+    Job: "Unity Game Developer - Enemy Type 3 Designer",
+    github: "https://github.com/KarthikeyanSugavanan",
+    linkdin: "https://www.linkedin.com/in/karthikeyan-sugavanan-01677718b",
+    insta: "",
+    email: "mailto:karthikeyan.sugavanan2019@vitstudent.ac.in",
+    imageUrl:
+      "https://lh3.googleusercontent.com/pw/ACtC-3fCmFsxzjQBMPxEnQQUwz4s7Kikc51BN65R4tt-C2glSys-dzgHSz3_HpupRgFePE7s2cS5uDGda5VgCbejwKxj6u0kObrrNdW7M4I9hweDbGObM4XJ1fQIqOXYy9HIx6kD489GE-LmZzNgiHudlmoB",
+  }
 ];
 export default team_members;
